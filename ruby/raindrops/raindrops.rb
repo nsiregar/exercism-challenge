44c752c63d90b6e1745089a1fc0c7ffcd25b6b96
Ngalim Siregar <ngalim.siregar@gmail.com>
2022-09-02 00:45:06 +0700

    [REFACTOR][RUBY] Raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,25 +1,36 @@
 # frozen_string_literal: true
 
 class Raindrops
-  MSG = {
+  MESSAGE = {
     3 => 'Pling',
     5 => 'Plang',
     7 => 'Plong'
   }.freeze
+  FACTOR_NUMBERS = [3, 5, 7].freeze
 
   def self.convert(number)
-    if factors(number).any? { |number| [3, 5, 7].include? number }
-      factors(number).map { |number| factor_pair(number) }.join
+    new(number).convert
+  end
+
+  def initialize(number)
+    @number = number
+  end
+
+  def convert
+    if factors(@number).any? { |num| FACTOR_NUMBERS.include? num }
+      factors(@number).map { |num| factor_pair(num) }.join
     else
-      factors(number).max.to_s
+      factors(@number).max.to_s
     end
   end
 
-  def self.factor_pair(number)
-    MSG[number]
+  private
+
+  def factor_pair(number)
+    MESSAGE[number]
   end
 
-  def self.factors(number)
+  def factors(number)
     (1..number).select { |n| (number % n).zero? }
   end
 end
